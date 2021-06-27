5420b013d3e0eddb1bebcd5141259e49a4e50dc0
Carson <carson0994@163.com>
2021-06-27 14:56:21 +0800

    push commit2

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -6,5 +6,6 @@ public class Test1 {
         System.out.println("hello git4");
         System.out.println("master ");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
